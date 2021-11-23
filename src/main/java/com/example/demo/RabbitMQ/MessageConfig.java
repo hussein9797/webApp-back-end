7dbc40db54f52639d8992989e3d8fa4c2897e17d
hussain.61 <husseinyousf505@gmail.com>
2021-11-23 14:59:59 +0200

    Preparing the project to develop  it at my work with h2 in memory   database

---
+++ src/main/java/com/example/demo/RabbitMQ/MessageConfig.java
@@ -0,0 +1,37 @@
+package com.example.demo.RabbitMQ;
+
+import com.rabbitmq.client.impl.AMQImpl;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MessageConfig {
+
+public final static String  QUEUE="Estates_queue";
+public final static String  EXCHANGE="Estates_Management_Exchange";
+public final static String  ROUTING_KEY="Estates_Routing_key";
+
+
+    @Bean
+    public Queue queue(){
+
+        return  new Queue(QUEUE);
+    }
+    @Bean
+    public TopicExchange exchange(){
+
+        return  new TopicExchange(EXCHANGE);
+
+
+    }
+    @Bean
+    public Binding binding(Queue queue,TopicExchange exchange){
+
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    }
+
+}
